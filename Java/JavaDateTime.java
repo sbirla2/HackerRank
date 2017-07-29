3790ff08de0a67f563e00cad67eb7f40900b958a
Gajesh Bhat <gajeshbht@gmail.com>
2017-07-29 19:00:25 +0530

    Organizing

--- Java/JavaDateTime.java
+++ Java/JavaDateTime.java
@@ -2,20 +2,23 @@ import java.io.*;
 import java.util.*;
 import java.text.*;
 import java.math.*;
-import java.util.regex.*;
+import java.time.*;
 
 public class Solution {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        String month = in.next();
+        String day = in.next();
+        String year = in.next();
+        in.close();
 
-        String inputDate = in.nextLine();
-        inputDate.replaceAll("","/");
+        DateFormatSymbols dfs = new DateFormatSymbols(Locale.US);
+        String weekdays[] = dfs.getWeekdays();
 
-        DateFormat formatter = new SimpleDateFormat("MM/dd/yy");
-        Date ipdate = formatter.parse(inputDate);
+        Calendar cal = new GregorianCalendar(Locale.US);
+        cal.set(Integer.parseInt(year), Integer.parseInt(month)-1, Integer.parseInt(day));
 
-        SimpleDateFormat dateObj = new SimpleDateFormat("EEEE"); // the day of the week spelled out completely
-        System.out.println(dateObj.format(ipdate));
+        System.out.println(weekdays[cal.get(Calendar.DAY_OF_WEEK)].toUpperCase());
     }
 }
